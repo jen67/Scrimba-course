b40f4568b464228d44b3c908acd9d4e03c2267a0
jen67 <amakrigift2000@gmail.com>
2024-02-10 23:13:17 +0100

    done

--- Figma-to-code-challenges/data-analytics-dashboard/index.js
+++ Figma-to-code-challenges/data-analytics-dashboard/index.js
@@ -1,1 +1,13 @@
-// javascript
+const openbtn = document.querySelector(".openMenu");
+const closebtn = document.querySelector(".closeMenu");
+const nav = document.querySelector("nav");
+
+closebtn.addEventListener("click", () => {
+  nav.style.transform = "translateX(0)";
+  alert("close");
+});
+
+openbtn.addEventListener("click", () => {
+  nav.style.transform = "translateX(100%)";
+  alert("open");
+});
